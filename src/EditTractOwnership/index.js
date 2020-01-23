c875e73d81b8a18e7642508b6013042da412c708
Lexa <lexaraea@gmail.com>
2020-01-23 17:42:41 -0500

    Interview

--- src/EditTractOwnership/index.js
+++ src/EditTractOwnership/index.js
@@ -1,204 +1,242 @@
-// import React from 'react';
-// import uuidv4 from 'uuid/v4';
-
-// const EditTractOwnership = ({ value = [], onChange = () => {} }) => {
-//   return <h2>TODO</h2>;
-// };
 import React from 'react';
 import uuidv4 from 'uuid/v4';
-
-import EditTractOwnership from './';
-
+import Icon from '../Icon/index';
 //
-  Add Mineral Interest, () => {
-    const value = [
-      {
-        id: uuidv4(),
-        owner: 'Luke Skywalker',
-        interest: '50',
-        lease: 'Tatooine Lease',
-        npris: [],
-      },
-      {
-        id: uuidv4(),
-        owner: 'Leia Organa',
-        interest: '5',
-        lease: 'Alderaan Lease',
-        npris: [],
-      },
-    ];
 
-    render(<EditTractOwnership value={value} />);
+const TractOwnershipHeader = () => (
+  <thead>
+    <tr>
+      <th>Owner</th>
+      <th>Mineral Interest</th>
+      <th>NPRI</th>
+      <th>Lease</th>
+    </tr>
+  </thead>
+);
 
-    const lukeMineralInterest = (
-      `mineralInterest-${value[0].id}`
-    );
-
-    const leiaMineralInterest = (
-      `mineralInterest-${value[1].id}`
-    );
-  });
-
+const EditTractOwnership = ({ value = [], onChange = () => {} }) => {
+  return (
+    <table>
+      <TractOwnershipHeader />
+      <tbody>
+        <tr>
+          <td>Luke Skywalker</td>
+          <td>0.5</td>
+          <td></td>
+          <td>Tatooine Lease</td>
+        </tr>
+        <tr>
+          <td>
+            <Icon icon="indent" />
+            <span>Leia Organa</span>
+          </td>
+          <td></td>
+          <td>.45</td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
 //
-  Render Mineral Interest, () => {
-    const value = [
-      {
-        id: uuidv4(),
-        owner: 'Luke Skywalker',
-        interest: '50',
-        lease: 'Tatooine Lease',
-        npris: [
-          {
-            id: uuidv4(),
-            owner: 'Han Solo',
-            interest: '10',
-          },
-        ],
-      },
-      {
-        id: uuidv4(),
-        owner: 'Leia Organa',
-        interest: '5',
-        lease: 'Alderaan Lease',
-      },
-    ];
-
-    render(<EditTractOwnership value={value} />);
-
-    const lukeMineralInterest = (
-      `mineralInterest-${value[0].id}`
-    );
-
-    const leiaMineralInterest = (
-      `mineralInterest-${value[1].id}`
-    );
-
-    const hanNPRI = (`npri-${value[0].npris[0].id}`);
-
-  });
-
 //
-  Add Empty Mineral Interest Row, () => {
-    let result;
-
-    render(<EditTractOwnership onChange={v => (result = v)} />);
-
-    const addMineralInterest = ('Add Mineral Interest');
-    onChange(addMineralInterest);
-  });
-
+// import React, { Component } from 'react'
+// // import React from 'react';
+// import uuidv4 from 'uuid/v4';
 //
-  Add Empty Mineral Interest Row, () => {
-    let result;
-
-    render(<EditTractOwnership onChange={v => (result = v)} />);
-
-    const addMineralInterest = ('Add Mineral Interest');
-    onChange(addMineralInterest);
-
-    const id = result[0].id;
-
-    const ownerInput = (`mineralInterest-${id}.owner`);
-    onChange(ownerInput, { target: { value: 'Luke Skywalker' } });
-
-    const interestInput = (`mineralInterest-${id}.interest`);
-    onChange(interestInput, { target: { value: '40' } });
-
-    const leaseInput = (`mineralInterest-${id}.lease`);
-    onChange(leaseInput, { target: { value: 'Tatooine Lease' } });
-  });
-
-
+// import EditTractOwnership from './';
 //
-  Add And Update Mineral Interest + NPRI row, () => {
-    let result;
-
-    render(<EditTractOwnership onChange={v => (result = v)} />);
-
-    const addMineralInterest = ('Add Mineral Interest');
-    onChange(addMineralInterest);
-
-    const addNPRI = ('Add NPRI');
-    onChange(addNPRI);
-
-    const id = result[0].id;
-
-    const ownerInput = (`mineralInterest-${id}.owner`);
-    onChange(ownerInput, { target: { value: 'Luke Skywalker' } });
-
-    const interestInput = (`mineralInterest-${id}.interest`);
-    onChange(interestInput, { target: { value: '40' } });
-
-    const leaseInput = (`mineralInterest-${id}.lease`);
-    onChange(leaseInput, { target: { value: 'Tatooine Lease' } });
-
-    const npriId = result[0].npris[0].id;
-
-    const npriOwnerInput = (`npri-${npriId}.owner`);
-    onChange(npriOwnerInput, { target: { value: 'Han Solo' } });
-
-    const npriInterestInput = (`npri-${npriId}.interest`);
-    onChange(npriInterestInput, { target: { value: '10' } });
-  });
-
-
 //
-  Remove Mineral Interest Row, () => {
-    let result;
-    const value = [
-      {
-        id: uuidv4(),
-        owner: 'Luke Skywalker',
-        interest: '50',
-        lease: 'Tatooine Lease',
-      },
-      {
-        id: uuidv4(),
-        owner: 'Leia Organa',
-        interest: '5',
-        lease: 'Alderaan Lease',
-      },
-    ];
-
-    render(<EditTractOwnership value={value} onChange={v => (result = v)} />);
-
-    const id = value[0].id;
-    const removeButton = (`mineralInterest-${id}.remove`);
-    onChange(removeButton);
-  });
-
-
+// const value = [
+//   {
+//     id: uuidv4(),
+//     owner: 'Luke Skywalker',
+//     interest: '50',
+//     lease: 'Tatooine Lease',
+//     npris: [],
+//   },
+//   {
+//     id: uuidv4(),
+//     owner: 'Leia Organa',
+//     interest: '5',
+//     lease: 'Alderaan Lease',
+//     npris: [],
+//   },
+// ];
 //
-  Remove NPRI Row, () => {
-    let result;
-    const value = [
-      {
-        id: uuidv4(),
-        owner: 'Luke Skywalker',
-        interest: '50',
-        lease: 'Tatooine Lease',
-        npris: [
-          {
-            id: uuidv4(),
-            owner: 'Han Solo',
-            interest: '10',
-          },
-        ],
-      },
-      {
-        id: uuidv4(),
-        owner: 'Leia Organa',
-        interest: '5',
-        lease: 'Alderaan Lease',
-      },
-    ];
-
-    render(<EditTractOwnership value={value} onChange={v => (result = v)} />);
-
-    const id = value[0].npris[0].id;
-    const removeButton = (`npri-${id}.remove`);
-    onChange(removeButton);
-  });
-});
-
+// class AddMineralInterest extends Component {
+//   render () {
+//     return
+//     {value.id};
+//
+//     const lukeMineralInterest = (
+//       `mineralInterest-${value[0].id}`
+//     );
+//
+//     const leiaMineralInterest = (
+//       `mineralInterest-${value[1].id}`
+//     );
+// };
+//
+//
+//   RenderMineralInterest, () => {
+//     const value = [
+//       {
+//         id: uuidv4(),
+//         owner: 'Luke Skywalker',
+//         interest: '50',
+//         lease: 'Tatooine Lease',
+//         npris: [
+//           {
+//             id: uuidv4(),
+//             owner: 'Han Solo',
+//             interest: '10',
+//           },
+//         ],
+//       },
+//       {
+//         id: uuidv4(),
+//         owner: 'Leia Organa',
+//         interest: '5',
+//         lease: 'Alderaan Lease',
+//       },
+//     ];
+//
+//     render(<EditTractOwnership value={value} />);
+//
+//     const lukeMineralInterest = (
+//       `mineralInterest-${value[0].id}`
+//     );
+//
+//     const leiaMineralInterest = (
+//       `mineralInterest-${value[1].id}`
+//     );
+//
+//     const hanNPRI = (`npri-${value[0].npris[0].id}`);
+//   });
+//
+// //
+//   AddEmptyMineralInterestRow, () => {
+//     let result;
+//
+//     render(<EditTractOwnership onChange={v => (result = v)} />);
+//
+//     const addMineralInterest =
+//     onChange(addMineralInterest);
+//   });
+//
+// //
+//   AddEmptyMineralInterestRow, () => {
+//     let result;
+//
+//     render(<EditTractOwnership onChange={v => (result = v)} />);
+//
+//     const addMineralInterest =
+//     onChange(addMineralInterest);
+//
+//     const id = result[0].id;
+//
+//     const ownerInput =
+//     onChange(ownerInput, { target: { value: 'Luke Skywalker' } });
+//
+//     const interestInput =
+//     onChange(interestInput, { target: { value: '40' } });
+//
+//     const leaseInput =
+//     onChange(leaseInput, { target: { value: 'Tatooine Lease' } });
+//   });
+//
+//
+// //
+//   AddAndUpdateMineralInterest + NPRI row, () => {
+//     let result;
+//
+//     render(<EditTractOwnership onChange={v => (result = v)} />);
+//
+//     const addMineralInterest =
+//     onChange(addMineralInterest);
+//
+//     const addNPRI =
+//     onChange(addNPRI);
+//
+//     const id = result[0].id;
+//
+//     const ownerInput =
+//     onChange(ownerInput, { target: { value: 'Luke Skywalker' } });
+//
+//     const interestInput =
+//     onChange(interestInput, { target: { value: '40' } });
+//
+//     const leaseInput =
+//     onChange(leaseInput, { target: { value: 'Tatooine Lease' } });
+//
+//     const npriId = result[0].npris[0].id;
+//
+//     const npriOwnerInput =
+//     onChange(npriOwnerInput, { target: { value: 'Han Solo' } });
+//
+//     const npriInterestInput =
+//     onChange(npriInterestInput, { target: { value: '10' } });
+//   });
+//
+//
+// //
+//   Remove Mineral Interest Row, () => {
+//     let result;
+//     const value = [
+//       {
+//         id: uuidv4(),
+//         owner: 'Luke Skywalker',
+//         interest: '50',
+//         lease: 'Tatooine Lease',
+//       },
+//       {
+//         id: uuidv4(),
+//         owner: 'Leia Organa',
+//         interest: '5',
+//         lease: 'Alderaan Lease',
+//       },
+//     ];
+//
+//     render(<EditTractOwnership value={value} onChange={v => (result = v)} />);
+//
+//     const id = value[0].id;
+//     const removeButton =
+//     onChange(removeButton);
+//   });
+//
+//
+// //
+//   Remove NPRI Row, () => {
+//     let result;
+//     const value = [
+//       {
+//         id: uuidv4(),
+//         owner: 'Luke Skywalker',
+//         interest: '50',
+//         lease: 'Tatooine Lease',
+//         npris: [
+//           {
+//             id: uuidv4(),
+//             owner: 'Han Solo',
+//             interest: '10',
+//           },
+//         ],
+//       },
+//       {
+//         id: uuidv4(),
+//         owner: 'Leia Organa',
+//         interest: '5',
+//         lease: 'Alderaan Lease',
+//       },
+//     ];
+//
+//     render(<EditTractOwnership value={value} onChange={v => (result = v)} />);
+//
+//     const id = value[0].npris[0].id;
+//     const removeButton =
+//     onChange(removeButton);
+//   });
+// });
+//
 export default EditTractOwnership;
